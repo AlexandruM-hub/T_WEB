c2aaa6b549d7ab0602dfa0ce449cb4d39d17ba42
Mazureac Alexandru <alexandru.mazureac@iis.utm.md>
2021-10-25 10:47:46 +0300

    Menu bar modification

--- Lab.1/js/menu_bar.js
+++ Lab.1/js/menu_bar.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.MenuOptions');
 
-menu.addEventListener('click', function(){
+menu.addEventListener('mouseenter', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+menu.addEventListener('mouseleave', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
