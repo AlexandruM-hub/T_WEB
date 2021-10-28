88e226a07573301fa97fd7739038c8fd3cc787f7
Mazureac Alexandru <alexandru.mazureac@iis.utm.md>
2021-10-28 23:56:09 +0300

    Log in was added

--- Lab.1/js/menu_bar.js
+++ Lab.1/js/menu_bar.js
@@ -1,12 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.MenuOptions');
 
-menu.addEventListener('mouseenter', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
-
-menu.addEventListener('mouseleave', function(){
+menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
