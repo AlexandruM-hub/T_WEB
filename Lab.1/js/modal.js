c0fd0f54bea7fa3a2e122f8df327b00d31305ffb
Mazureac Alexandru <alexandru.mazureac@iis.utm.md>
2021-10-29 17:06:33 +0300

    Register form & Mail form were added!

--- Lab.1/js/modal.js
+++ Lab.1/js/modal.js
@@ -1,8 +1,12 @@
 
 var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+var modal3 = document.getElementById('id03');
 
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal || event.target == modal2 || event.target == modal3) {
     modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
   }
 }
